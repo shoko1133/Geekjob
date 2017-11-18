8e85420d0c4dedc77ee6ca335144bc4897d1fa34
shochan <s.higashida.2222@gmail.com>
2017-11-18 14:10:35 +0900

    クラス作成課題提出２

--- CampChallenge_java/013.クラス/Test.java
+++ CampChallenge_java/013.クラス/Test.java
@@ -8,30 +8,25 @@
  *
  * @author shochan
  */class Human {
-    // フィールドの宣言
+    
     public String name = "";
     public int age = 0;
 
-    // メソッドの宣言
     public void setHuman(String n, int a) {
-        // 引数をフィールドへ設定
+        
         this.name = n;
         this.age = a;
     }
     }
- 
- 
- 
+
     public class Test {
      public static void main(String[] args) {
-        // Humanクラスのインスタンス生成
+        
         Human hito = new Human();
 
-        // 人インスタンスのageフィールドへ年齢設定
-        hito.age = 40;
-
-        // 人インスタンスのsetHumanメソッドを利用
-        hito.setHuman("添田亮司", 34);
+        hito.age = 25;
+        hito.name=("higashida");
+        
     }
     }
     
